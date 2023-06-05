4312ca116bfa7e08e9bc97d24d3a936b4ba9e647
mikaeltreppo <mikaeltreppo@outlook.com>
2023-06-05 16:55:23 +0200

    rotte iniziate

--- router.js
+++ router.js
@@ -1,1 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
+import AppHome from './pages/AppHome.vue';
+
+const router = createRouter(
+    {
+        history:createWebHistory(),
+        routes:[
+            {
+                path: '/',
+                name: 'home',
+                component: AppHome
+            },
+
+        ]
+    });
+
+    export {router};
---
+++ src/pages/AppHome.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <MyCard></MyCard>
+    </div>
+</template>
+<script>
+
+import axios from 'axios';
+import { store } from '../../store';
+import MyCard from '../components/MyCard.vue';
+
+export default {
+    components: {
+        MyCard,
+
+    },
+    data() {
+        return {
+            cars: [],
+            store,
+            currentPage: 1,
+            lastPage: null
+        }
+    },
+    methods: {
+        getCard() {
+            axios.get(`${this.store.baseUrl}/api/cars`).then(response => {
+
+                this.cars = response.data.results.data;
+
+                console.log(this.cars)
+            });
+         }}}
+</script>
+<style lang="scss"></style>

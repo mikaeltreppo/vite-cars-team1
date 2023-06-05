4371e1138ace275a4f2077a9b9725345d0ab5bfb
Valerio Salzano <vlr.slzn@gmail.com>
2023-06-05 17:27:54 +0200

    modificata mycard

--- src/components/MyCard.vue
+++ src/components/MyCard.vue
@@ -2,18 +2,21 @@
 //<div class="card m-3"  style="width: 18rem;">
     <div class="card-body">
         <img src="https://picsum.photos/200/300" class="card-img-top" alt="...">
-        <h5 class="card-title">{{ car.brand}}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">{{ car.author }}</h6>
-        <p class="card-text">{{ car.description }}</p>
-        <router-link :to="{name: 'singleproject', params: {id: car.id}}" class="btn btn-primary">
+        <h5 class="card-title">{{ data.brand}}</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">{{ data.model }}</h6>
+        <p class="card-text">{{ data.price }}</p>
+        <!-- <router-link :to="{name: 'singleproject', params: {id: car.id}}" class="btn btn-primary">
             Vedi post completo
-        </router-link>
+        </router-link> -->
 
     </div>
 </div>
 </template>
 <script>
 export default{
-    name: 'MyCard'
+    name: 'MyCard',
+    props: {
+        data: Array,
+    }
 }
 </script>
